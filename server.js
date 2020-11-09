b0e2002c075ba77b216450e76103a4e35e6b139c
CHR <harshchakalasiya8316@gmail.com>
2020-11-09 16:58:47 +0530

    Second Commit

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
-const express = require('express')
+
+require('dotenv').config();
+const express = require('express');
 const app = express();
-const path = require('path')
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
+const path = require('path');
+const cors = require('cors');
+// Cors 
+const corsOptions = {
+  origin: process.env.ALLOWED_CLIENTS.split(',')
 
-app.use(express.static('public'))
-app.use(express.json())
-const connectDB = require('./config/db.js');
+app.use(cors(corsOptions))
+app.use(express.static('public'));
+
+const connectDB = require('./config/db');
 connectDB();
-// template engine
 
-app.set('views', path.join(__dirname,'/views'));
-app.set('view engine','ejs')
+app.use(express.json());
 
-// Routes
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
 
-app.use('/api/files',require('./routes/files'))
+// Routes 
+app.use('/api/files', require('./routes/files'));
+app.use('/files', require('./routes/show'));
+app.use('/files/download', require('./routes/download'));
 
-app.use('/files',require('./routes/show'))
 
-app.use('/files/download',require('./routes/download'))
-app.listen(PORT,()=>{
-	console.log(`Listening on PORT ${PORT}`)
-})
-
+app.listen(PORT, console.log(`Listening on port ${PORT}.`));
