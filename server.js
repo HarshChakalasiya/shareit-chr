75a5a6ad850f3c20b9447ef3c64cb7b6c7737fc6
HarshChakalasiya <60564962+HarshChakalasiya@users.noreply.github.com>
2020-11-12 10:21:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,15 +14,17 @@ const connectDB = require('./config/db');
 connectDB();
 // // Cors 
 
-// const corsOptions ={
+ const corsOptions ={
 // 	// origin: ['http://localhost:3000','http://localhost:5000','http://localhost:3300']
-// // 	origin: process.env.ALLOWED_CLIENTS.split(',')
+ 	origin: process.env.ALLOWED_CLIENTS.split(',')
 // 	origin: ['http://hy-news.in/shareIT/']
-// }
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+ }
+ 
+ app.use(cors(corsOptions))
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   next();
+// });
 
 
 app.use(express.json());
